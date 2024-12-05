0e0a5549c971e9870501e75e4fb8f73086ac624f
Micro-Organism <coolrabbit1993@gmail.com>
2024-12-05 08:47:23 +0800

    feat: springboot 集成 puppeteer

---
+++ zero-puppeteer-boot/src/main/java/com/zero/puppeteer/ZeroPuppeteerBootApplication.java
@@ -0,0 +1,13 @@
+package com.zero.puppeteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroPuppeteerBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroPuppeteerBootApplication.class, args);
+    }
+
+}
---
+++ zero-puppeteer-boot/src/test/java/com/zero/puppeteer/ZeroPuppeteerBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.puppeteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroPuppeteerBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
